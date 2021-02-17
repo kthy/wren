204190ac740e9812af20f16b5627d2631b2d213c
Kristian Thy <kth@miracle.dk>
2021-02-17 16:07:12 +0100

    feat: add working directory to config

--- wren/cli.py
+++ wren/cli.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """CLI entrypoint."""
 
-from glob import glob
-from os.path import join
-from re import search
+from os.path import exists as path_exists
 
 import click
 from config import Config
@@ -17,26 +15,29 @@ from config import Config
     "-c", "--config", default="./wren.cfg", type=click.Path(exists=True), show_default=True
 )
 def rename_ships(changesets, config):
-    """This is a doc comment."""
+    """Main CLI entrypoint."""
 
-    click.echo(f"Using config from {click.format_filename(config)}")
-    cfg = Config(config)
-    click.echo(cfg.as_dict())
+    try:
+        click.echo(f"Using config from {click.format_filename(config)}")
+        cfg = Config(config)
+        click.echo(cfg.as_dict())
 
-    if len(changesets) == 0:
-        changesets = ["cyrillic", "joker", "radarWithRange"]
-    click.echo(f"Searching for {len(changesets)} changesets")
-    for changeset in changesets:
-        changeset_path = f"changesets.{changeset}"
-        click.echo(f'Processing changeset "{changeset_path}"')
-        click.echo(cfg[changeset_path])
+        workdir = cfg["workingDirectory"]
+        if not path_exists(workdir):
+            raise click.ClickException(
+                f"Invalid value for 'workingDirectory': Path '{workdir}' does not exist"
+            )
+        click.echo(f"Working directory {workdir}/wren-{run_id}")
+
+        # Set default changeset
+        if len(changesets) == 0:
+            changesets = ["cyrillic", "joker", "radarWithRange"]  # TODO: only radarWithRange
+        for changeset in changesets:
+            process_changeset(cfg[f"changesets.{changeset}"])
+    except Exception as err:
+        raise err
 
 
-def glob_files(directory):
-    """Return list of non-excluded files in dir and its subdirs."""
-    return [f for f in glob(join(directory, "**", "*.cs"), recursive=True) if not exclude(f)]
-
-
-def exclude(path):
-    """Return True if the path should be excluded."""
-    return search(r"AssemblyInfo\.cs|Test\.cs|/(bin|obj)/(Debug|Release)/", path)
+def process_changeset(changeset_config):
+    """Process a changeset."""
+    click.echo(changeset_config)
