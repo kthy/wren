a03378ab428d9f261257fbc79e2796225b0884e8
Kristian Thy <kth@miracle.dk>
2021-02-17 01:49:40 +0100

    qa: add test canary

---
+++ tests/test_cli.py
@@ -0,0 +1,18 @@
+"""Test the wren CLI."""
+
+# from pytest import fixture
+
+# from wren.cli import ()
+
+
+# @fixture
+# def client():
+#     """Yield client fixture.
+#     See <http://flask.pocoo.org/docs/1.0/testing/#the-testing-skeleton>
+#     for ideas to expand this.
+#     """
+#     yield backend.PLATEYPUS.test_client()
+
+def test_tautology():
+    """This test canary always passes."""
+    assert 2 + 2 == 4
