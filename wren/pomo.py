5e8421322cd35ede45be114514d6a8daba427a80
Kristian Thy <kth@miracle.dk>
2021-02-17 23:43:02 +0100

    feat: implement backup/restore methods

--- wren/pomo.py
+++ wren/pomo.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 """Gettext manipulation methods."""
 
-def backup_original_mo(wowsdir):
-    """Rename the original `global.mo` to `global.mo.original`."""
-    # TODO: Checksum file after moving.
-    raise NotImplementedError
+from os import remove
+from shutil import copyfile
 
+from filehash import FileHash
+
+LC = "res/texts/en/LC_MESSAGES"
+
+def backup_original_mo(wowsdir):
+    """Copy the original `global.mo` to `global.mo.original`."""
+    global_mo_path = f"{wowsdir}/{LC}/global.mo"
+    backup_mo_path = f"{global_mo_path}.original"
+    copyfile(global_mo_path, backup_mo_path)
+    hasher = FileHash("md5")
+    if hasher.hash_file(global_mo_path) != hasher.hash_file(backup_mo_path):
+        raise OSError("Copy failed, hash mismatch detected")
 
 def compile_mo(workdir):
     """Convert `workdir/global.po` using `msgfmt` into `workdir/global.mo`"""
@@ -22,7 +32,12 @@ def prepare_po(workdir):
     raise NotImplementedError
 
 
-def restore_original_mo(workdir, wowsdir):
+def restore_original_mo(wowsdir):
     """Reinstate the original `global.mo` from `global.mo.original`."""
-    # TODO: Checksum file after moving.
-    raise NotImplementedError
+    global_mo_path = f"{wowsdir}/{LC}/global.mo"
+    backup_mo_path = f"{global_mo_path}.original"
+    copyfile(backup_mo_path, global_mo_path)
+    hasher = FileHash("md5")
+    if hasher.hash_file(global_mo_path) != hasher.hash_file(backup_mo_path):
+        raise OSError("Copy failed, hash mismatch detected")
+    remove(backup_mo_path)
