a60fc6b9f856c7bd80cdef15f452886b9922909d
Kristian Thy <kth@miracle.dk>
2021-02-18 16:35:45 +0100

    qa: improve test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,9 +8,13 @@ from fixtures import fixture_wowsdir  # pylint: disable=unused-import
 
 from wren.cli import _bindir
 
+
 def test__bindir(wowsdir):
     """Test the _bindir method."""
     bin_path = abspath(wowsdir + "/..")
+    assert _bindir(bin_path) == "7777777"
     for subdir in ("1111111", "2222222", "3333333", "4444444", "5555555", "6666666"):
         makedirs(f"{bin_path}/{subdir}")
     assert _bindir(bin_path) == "7777777"
+    makedirs(f"{bin_path}/8888888")
+    assert _bindir(bin_path) == "8888888"
