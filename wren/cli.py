ba391f34a64f97fdba20e30930475cb4f080b9e7
Kristian Thy <kth@miracle.dk>
2021-02-19 01:30:19 +0100

    feat: implement working procedure

--- wren/cli.py
+++ wren/cli.py
@@ -4,21 +4,12 @@
 from os import walk
 from os.path import abspath, basename, exists as path_exists
 from re import ASCII, match
-from tempfile import TemporaryDirectory
 
 import click
 from config import Config
 
 from wren.change import Change
-from wren.pomo import (
-    backup_original_mo,
-    compile_mo,
-    install_modified_mo,
-    prepare_po,
-    restore_original_mo,
-)
-
-# from subprocess import CalledProcessError, run  # nosec
+from wren.pomo import apply_changes, backup_original_mo, get_mo, restore_original_mo
 
 HELP_CFG = "Path to config file."
 HELP_UNDO = "If provided, the original names will be reinstated."
@@ -48,50 +39,43 @@ def rename_ships(changesets, config, undo):
 
     try:
         cfg = load_config(config)
-        with get_working_directory(cfg) as workdir:
-            wowsdir = get_warship_directory(cfg)
+        wowsdir = get_warship_directory(cfg)
 
-            if undo:
-                restore_original_mo(wowsdir)
-                return
+        if undo:
+            restore_original_mo(wowsdir)
+            return
 
-            if len(changesets) == 0:
-                changesets = get_default_changeset()
+        change_prefix = []
+        change_replace = []
 
-            backup_original_mo(wowsdir)
+        for changeset in changesets or default_changesets():
+            try:
+                changelist = [Change(**change) for change in cfg[f"changesets.{changeset}"]]
+                change_prefix.extend(filter(lambda c: c.is_prefix(), changelist))
+                change_replace.extend(filter(lambda c: c.is_replace(), changelist))
+            except KeyError:
+                click.echo(f"WARNING: Changeset {changeset} not found in configuration.")
+                continue
 
-            prepare_po(workdir)
+        if len(change_prefix) + len(change_replace) == 0:
+            click.echo("No changes to apply.")
+            return
 
-            for changeset in changesets:
-                apply_changeset(workdir, cfg[f"changesets.{changeset}"])
+        backup_original_mo(wowsdir)
 
-            compile_mo(workdir)
-
-            install_modified_mo(workdir, wowsdir)
+        mo_file = get_mo(wowsdir)
+        apply_changes(mo_file, change_replace)
+        apply_changes(mo_file, change_prefix)
+        mo_file.save()
     except Exception as err:
         click.echo("Aborting…")
         restore_original_mo(wowsdir)
         raise err
 
 
-def apply_change(change):
-    """Apply a change."""
-    print(change)
-    raise NotImplementedError
-
-
-def apply_changeset(workdir, changeset_config):
-    """Apply all changes in a changeset."""
-    # TODO: open PO file.
-    changes = [Change(**change) for change in changeset_config]
-    for change in changes:
-        apply_change(change)
-
-
-def get_default_changeset():
-    """Get the default changeset."""
-    # TODO: Should only return radarWithRange when done.
-    return ["cyrillic", "joker", "radarWithRange"]
+def default_changesets():
+    """Get the default changesets."""
+    return ["radarWithRange"]
 
 
 def get_warship_directory(cfg):
@@ -102,16 +86,6 @@ def get_warship_directory(cfg):
     wowsdir = abspath(f"{wows_path}/{bindir}")
     raise_if_not_path_exists(cfg_key, wowsdir)
     return wowsdir
-
-
-def get_working_directory(cfg):
-    """Get a string with the path to the working directory."""
-    cfg_key = "workingDirectory"
-    workdir_base = cfg[cfg_key]
-    raise_if_not_path_exists(cfg_key, workdir_base)
-    workdir = TemporaryDirectory(prefix="wren-", dir=workdir_base)
-    click.echo(f"Working directory {workdir.name}")
-    return workdir
 
 
 def load_config(config_path):
