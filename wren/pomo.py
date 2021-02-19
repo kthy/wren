9171e6cee1c7dc2b809c92aa663cd4d4667e4234
Kristian Thy <kth@miracle.dk>
2021-02-19 01:29:39 +0100

    feat: implement apply_changes, remove unused methods

--- wren/pomo.py
+++ wren/pomo.py
@@ -5,8 +5,17 @@ from os import remove
 from shutil import copyfile
 
 from filehash import FileHash
+from polib import MOFile, mofile
+
+from wren.change import Change
 
 LC = "res/texts/en/LC_MESSAGES"
+
+
+def apply_changes(mo_file: MOFile, changelist: list[Change]):
+    """Apply all changes in the provided list of changes to the given MOFile."""
+    for change in changelist:
+        change.apply(mo_file)
 
 
 def backup_original_mo(wowsdir):
@@ -16,20 +25,9 @@ def backup_original_mo(wowsdir):
     _copyfile_and_checksum(global_mo_path, backup_mo_path)
 
 
-def compile_mo(workdir):
-    """Convert `workdir/global.po` using `msgfmt` into `workdir/global.mo`"""
-    raise NotImplementedError
-
-
-def install_modified_mo(workdir, wowsdir):
-    """Copy the new `global.mo` to where the original was."""
-    global_mo_path = _global_mo_path(wowsdir)
-    _copyfile_and_checksum(f"{workdir}/global.mo", global_mo_path)
-
-
-def prepare_po(workdir):
-    """Convert `${wowsPath}/res/texts/en/LC_MESSAGES/global.mo` to `workdir/global.po`"""
-    raise NotImplementedError
+def get_mo(wowsdir) -> MOFile:
+    """Open and return the global MO file in the given directory."""
+    return mofile(f"{wowsdir}/{LC}/global.mo")
 
 
 def restore_original_mo(wowsdir):
@@ -44,6 +42,7 @@ def _copyfile_and_checksum(from_path, to_path):
     """Copy a file from from_path to to_path.
 
     Raises OSError if the new file's checksum doesn't match the original."""
+    # TODO: Copy with metadata intact.
     copyfile(from_path, to_path)
     hasher = FileHash("md5")
     if hasher.hash_file(from_path) != hasher.hash_file(to_path):
