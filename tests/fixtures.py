47a44d98b85ab0f2996cd08a2648930c8b8b3019
Kristian Thy <thy@42.dk>
2021-02-22 02:01:59 +0100

    qa: more and better test coverage

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,28 +1,38 @@
 # -*- coding: utf-8 -*-
 """Test fixtures."""
 
-from os import makedirs
+from os import environ, makedirs
 from os.path import abspath, exists
 from shutil import copyfile, rmtree
 from tempfile import mkdtemp
 
+from config import Config
 from pytest import fixture
 
-BIN = "7777777"
-TEST_MO_PATH = "./tests/testdata/global.mo"
+TMPDIR = f"{mkdtemp(prefix='wren-pytest-')}"
+
+for b in ("1111111", "2222222", "3333333", "4444444", "5555555", "6666666", "7777777"):
+    makedirs(abspath(f"{TMPDIR}/bin/{b}"))
+
+
+@fixture(name="config")
+def fixture_config():
+    """Yield config object."""
+    environ["TMPDIR"] = TMPDIR + "\\bin"
+    cfg = Config("./tests/testdata/test.cfg")
+    yield cfg
 
 
 @fixture(name="wowsdir")
 def fixture_wowsdir():
     """Yield WoWs directory."""
-    tmpdir = f"{mkdtemp(prefix='wren-pytest-')}/{BIN}"
-    _prepare_global_mo(tmpdir)
-    yield tmpdir
-    rmtree(tmpdir)
+    _prepare_global_mo(f"{TMPDIR}\\bin\\7777777")
+    yield f"{TMPDIR}\\bin\\7777777"
+    rmtree(TMPDIR)
 
 
 def _prepare_global_mo(wowsbin):
     binlcdir = abspath(f"{wowsbin}/res/texts/en/LC_MESSAGES")
     makedirs(binlcdir)
-    copyfile(TEST_MO_PATH, f"{binlcdir}/global.mo")
+    copyfile("./tests/testdata/global.mo", f"{binlcdir}/global.mo")
     assert exists(f"{binlcdir}/global.mo")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,17 +4,56 @@
 from os import makedirs
 from os.path import abspath
 
-from fixtures import fixture_wowsdir  # pylint: disable=unused-import
+from click import ClickException
+from config import Config
+from fixtures import fixture_config, fixture_wowsdir  # pylint: disable=unused-import
+from pytest import raises
 
-from wren.cli import _bindir
+from wren.cli import (
+    _bindir,
+    default_changesets,
+    get_warship_directory,
+    load_config,
+    raise_if_not_path_exists,
+)
+
+
+def test_default_changesets():
+    """Test the default_changesets method."""
+    assert default_changesets() == ["radar"]
+
+
+def test_get_warship_directory(config, wowsdir):
+    """Test the get_warship_directory method."""
+    assert get_warship_directory(config) == wowsdir
+
+
+def test_load_config():
+    """Test the load_config method."""
+    cfg = load_config("./tests/testdata/test.cfg")
+    assert isinstance(cfg, Config)
+
+
+def test_raise_if_not_path_exists_no_raise(wowsdir):
+    """Test the raise_if_not_path_exists method (no error raised)."""
+    assert raise_if_not_path_exists("dummy", wowsdir) is None
+
+
+def test_raise_if_not_path_exists_raises(wowsdir):
+    """Test the raise_if_not_path_exists method (error raised)."""
+    with raises(ClickException):
+        raise_if_not_path_exists("dummy", f"{wowsdir}\\foobar")
 
 
 def test__bindir(wowsdir):
     """Test the _bindir method."""
     bin_path = abspath(wowsdir + "/..")
     assert _bindir(bin_path) == "7777777"
-    for subdir in ("1111111", "2222222", "3333333", "4444444", "5555555", "6666666"):
-        makedirs(f"{bin_path}/{subdir}")
-    assert _bindir(bin_path) == "7777777"
     makedirs(f"{bin_path}/8888888")
     assert _bindir(bin_path) == "8888888"
+
+
+def test__bindir_raises():
+    """Test when _bindir method raises ValueError."""
+    with raises(ValueError):
+        _bindir("C:\\")
